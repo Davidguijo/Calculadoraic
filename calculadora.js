5f12394e37e22124d8fe833e6450a44a3c9cf35b
Davidguijo <dguilop906@g.educaand.es>
2025-02-03 11:17:56 +0100

    Añadir configuracion de CI con Github Actions

--- calculadora.js
+++ calculadora.js
@@ -5,6 +5,18 @@ function suma(a, b) {
   function resta(a, b) {
     return a - b;
   }
+
+  function multiplicacion(a, b) {
+    return a * b;
+  }
+
+  function division(a, b) {
+    return a / b;
+  } 
+
+  function raizCuadrada(a) {
+    return Math.sqrt(a);
+  }
   
   
-  module.exports = { suma, resta };
+  module.exports = { suma, resta, multiplicacion, division, raizCuadrada };
--- calculadora.test.js
+++ calculadora.test.js
@@ -1,9 +1,21 @@
-const { sumar, restar } = require('./calculadora');
+const { suma, resta, multiplicacion, division, raizCuadrada } = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
-    expect(sumar(2, 3)).toBe(5);
+    expect(suma(2, 3)).toBe(5);
 });
 
 test('Resta 5 - 3 y devuelve 2', () => {
-    expect(restar(5, 3)).toBe(2);
+    expect(resta(5, 3)).toBe(2);
+});
+
+test('Multiplicacion 2 * 5 y devuelve 10', () => {
+    expect(multiplicacion(2, 5)).toBe(10);
+});
+
+test('Resta 20 / 10 y devuelve 2', () => {
+    expect(division(20, 10)).toBe(2);
+});
+
+test('Raiz cuadrada de 9 y devuelve 3', () => {
+    expect(raizCuadrada(9)).toBe(3);
 });
